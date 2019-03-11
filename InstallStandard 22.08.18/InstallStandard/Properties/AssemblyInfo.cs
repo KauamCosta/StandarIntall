132cad98e971a265807d95fa9680211b37455694
Kauam da Silva Costa <47905012+KauamCosta@users.noreply.github.com>
2019-03-11 20:08:37 -0300

    Revert "Adição de codigo do vista e dos demais"
    This reverts commit c641e04788ee0c9612084655f3aa243ddb1e853d.

---
+++ InstallStandard 22.08.18/InstallStandard/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Standard Install")]
+[assembly: AssemblyDescription("Instalador padrão para maquinas da DETEC Pinhais")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("i9 Group")]
+[assembly: AssemblyProduct("Standard Install")]
+[assembly: AssemblyCopyright("Kauam Costa Copyright ©  2018")]
+[assembly: AssemblyTrademark("Kauam da Silva Costa")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("10d4bc7b-c0dc-4a6f-a2fb-4d6b02bab257")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("pt-BR")]
+
